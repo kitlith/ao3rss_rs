c64872808d802c8c3ed83bce22060f23572a0e13
Kitlith <kitlith@kitl.pw>
2020-09-19 17:25:19 -0700

    Refactor scraping and rss generation into associated functions

--- src/main.rs
+++ src/main.rs
@@ -43,85 +43,137 @@ struct Chapter {
     post_notes: Option<String>,
 }
 
-fn parse_date_from_node(node: select::node::Node) -> Option<NaiveDate> {
-    NaiveDate::parse_from_str(&node.text(), "%Y-%m-%d").ok()
-}
+impl Work {
+    async fn scrape(work_id: u64) -> Result<Work, Box<dyn Error + Send + Sync>> {
+        let client = reqwest::Client::new();
 
-async fn scrape_work(work_id: u64) -> Result<Work, Box<dyn Error + Send + Sync>> {
-    let client = reqwest::Client::new();
-
-    /*let navigation = client
-    .get(&format!("https://archiveofourown.org/works/{}/navigate", work_id))
-    .send()
-    .and_then(|x| x.text())
-    .map_ok(|x| Document::from(x.as_str()));*/
-    let work = client
-        .get(&format!(
-            "https://archiveofourown.org/works/{}?view_full_work=true",
-            work_id
-        ))
+        /*let navigation = client
+        .get(&format!("https://archiveofourown.org/works/{}/navigate", work_id))
         .send()
         .and_then(|x| x.text())
-        .map_ok(|x| Document::from(x.as_str()));
-    let work = work.await?;
+        .map_ok(|x| Document::from(x.as_str()));*/
+        let work = client
+            .get(&format!(
+                "https://archiveofourown.org/works/{}?view_full_work=true",
+                work_id
+            ))
+            .send()
+            .and_then(|x| x.text())
+            .map_ok(|x| Document::from(x.as_str()));
+        let work = work.await?;
 
-    Ok(Work {
-        work_id,
-        title: work
-            .find(Name("h2").and(Class("title")))
-            .nth(0)
-            .map(|n| n.text())
-            .ok_or("Missing Work Title")?,
-        summary: work
-            .find(
-                Attr("id", "workskin")
-                    .child(Class("preface"))
-                    .descendant(Class("userstuff")),
+        Ok(Work {
+            work_id,
+            title: work
+                .find(Name("h2").and(Class("title")))
+                .nth(0)
+                .map(|n| n.text())
+                .ok_or("Missing Work Title")?,
+            summary: work
+                .find(
+                    Attr("id", "workskin")
+                        .child(Class("preface"))
+                        .descendant(Class("userstuff")),
+                )
+                .nth(0)
+                .map(|n| n.text()),
+            notes: None,      // TODO?
+            post_notes: None, // TODO?
+            publish_date: work
+                .find(Class("published").and(Name("dd")))
+                .nth(0)
+                .and_then(parse_date_from_node)
+                .ok_or("Missing Publish Date")?,
+            update_date: work
+                .find(Class("status").and(Name("dd")))
+                .nth(0)
+                .and_then(parse_date_from_node)
+                .ok_or("Missing Update Date")?,
+            chapters: work
+                .find(Attr("id", "chapters").child(Class("chapter")))
+                .map(|chapter_node| Chapter::scrape(chapter_node))
+                .collect::<Result<Vec<Chapter>, Box<dyn Error + Send + Sync>>>()?,
+        })
+    }
+
+    fn to_rss(self) -> rss::Channel {
+        // NOTE: This function makes use of unwrap after building all the items,
+        // but this will apparently never panic because all fields have default values.
+        rss::ChannelBuilder::default()
+            .title(self.title)
+            .description(self.summary.unwrap_or("AO3 Fic".to_string()))
+            .link(format!("https://archiveofourown.org/works/{}", self.work_id))
+            .pub_date(rfc822(self.publish_date))
+            .last_build_date(rfc822(self.update_date))
+            .generator(Some("https://github.com/kitlith/ao3rss_rs".to_string()))
+            .namespaces(
+                [(
+                    "content".to_string(),
+                    "http://purl.org/rss/1.0/modules/content/".to_string(),
+                )]
+                    .iter()
+                    .cloned()
+                    .collect::<HashMap<String, String>>(),
             )
-            .nth(0)
-            .map(|n| n.text()),
-        notes: None,      // TODO?
-        post_notes: None, // TODO?
-        publish_date: work
-            .find(Class("published").and(Name("dd")))
-            .nth(0)
-            .and_then(parse_date_from_node)
-            .ok_or("Missing Publish Date")?,
-        update_date: work
-            .find(Class("status").and(Name("dd")))
-            .nth(0)
-            .and_then(parse_date_from_node)
-            .ok_or("Missing Update Date")?,
-        chapters: work
-            .find(Attr("id", "chapters").child(Class("chapter")))
-            .map(|chapter_node| {
-                let title = chapter_node
-                    .find(Class("title"))
-                    .nth(0)
-                    .ok_or("Missing Chapter Title")?;
-                let chapter_link = title.find(Name("a")).nth(0).ok_or("Missing Chapter Link")?;
+            .items(
+                self.chapters
+                    .into_iter()
+                    .map(|chapter| {
+                        rss::ItemBuilder::default()
+                            .title(chapter.title)
+                            .link(chapter.link.clone())
+                            .description(chapter.summary)
+                            .content(chapter.content)
+                            .guid(
+                                rss::GuidBuilder::default()
+                                    .value(chapter.link)
+                                    .permalink(true)
+                                    .build()
+                                    .unwrap(),
+                            )
+                            .build()
+                            .unwrap()
+                    })
+                    .collect::<Vec<_>>(),
+            )
+            .build()
+            .unwrap()
+    }
+}
 
-                let title = title.text();
-                let chapter_link = chapter_link.attr("href").ok_or("Missing href?")?;
 
-                Ok(Chapter {
-                    title,
-                    link: format!("https://archiveofourown.org{}", chapter_link),
-                    summary: chapter_node
-                        .find(Class("summary").child(Class("userstuff")))
-                        .nth(0)
-                        .map(|node| node.inner_html()), // TODO: consider dropping the inner
-                    pre_notes: None,
-                    content: chapter_node
-                        .children()
-                        .find(|node| node.is(Class("userstuff")))
-                        .map(|node| node.inner_html())
-                        .ok_or("Missing content")?,
-                    post_notes: None,
-                })
-            })
-            .collect::<Result<Vec<Chapter>, Box<dyn Error + Send + Sync>>>()?,
-    })
+
+impl Chapter {
+    fn scrape(chapter_node: select::node::Node) -> Result<Self, Box<dyn Error + Send + Sync>> {
+        let title = chapter_node
+            .find(Class("title"))
+            .nth(0)
+            .ok_or("Missing Chapter Title")?;
+        let chapter_link = title.find(Name("a")).nth(0).ok_or("Missing Chapter Link")?;
+
+        let title = title.text();
+        let chapter_link = chapter_link.attr("href").ok_or("Missing href?")?;
+
+        Ok(Chapter {
+            title,
+            link: format!("https://archiveofourown.org{}", chapter_link),
+            summary: chapter_node
+                .find(Class("summary").child(Class("userstuff")))
+                .nth(0)
+                .map(|node| node.inner_html()), // TODO: consider dropping the inner
+            pre_notes: None,
+            content: chapter_node
+                .children()
+                .find(|node| node.is(Class("userstuff")))
+                .map(|node| node.inner_html())
+                .ok_or("Missing content")?,
+            post_notes: None,
+        })
+    }
+}
+
+fn parse_date_from_node(node: select::node::Node) -> Option<NaiveDate> {
+    NaiveDate::parse_from_str(&node.text(), "%Y-%m-%d").ok()
 }
 
 fn keepalive_future(
@@ -156,48 +208,10 @@ fn rfc822(date: NaiveDate) -> String {
 #[tokio::main]
 async fn main() {
     let work = warp::path!("work" / u64).and(warp::get()).map(|work_id| {
-        let fut = scrape_work(work_id).map_ok(|res| {
+        let fut = Work::scrape(work_id).map_ok(|work| {
             // generate rss feed
             // TODO: categories/tags?
-            let channel = rss::ChannelBuilder::default()
-                .title(res.title)
-                .description(res.summary.unwrap_or("AO3 Fic".to_string()))
-                .link(format!("https://archiveofourown.org/works/{}", res.work_id))
-                .pub_date(rfc822(res.publish_date))
-                .last_build_date(rfc822(res.update_date))
-                .generator(Some("https://github.com/kitlith/ao3rss_rs".to_string()))
-                .namespaces(
-                    [(
-                        "content".to_string(),
-                        "http://purl.org/rss/1.0/modules/content/".to_string(),
-                    )]
-                    .iter()
-                    .cloned()
-                    .collect::<HashMap<String, String>>(),
-                )
-                .items(
-                    res.chapters
-                        .into_iter()
-                        .map(|chapter| {
-                            rss::ItemBuilder::default()
-                                .title(chapter.title)
-                                .link(chapter.link.clone())
-                                .description(chapter.summary)
-                                .content(chapter.content)
-                                .guid(
-                                    rss::GuidBuilder::default()
-                                        .value(chapter.link)
-                                        .permalink(true)
-                                        .build()
-                                        .unwrap(),
-                                ) // TODO: unwrap
-                                .build()
-                                .unwrap() // TODO: unwrap
-                        })
-                        .collect::<Vec<_>>(),
-                )
-                .build()
-                .unwrap(); // TODO: unwrap
+            let channel = work.to_rss();
 
             // TODO: validate?
 
